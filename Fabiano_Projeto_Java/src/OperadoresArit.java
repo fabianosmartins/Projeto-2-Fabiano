c2d5d1ba671c9bbdba010813353b0b484c470f5f
Fabiano M <Fabiano M@DESKTOP-R89IUIV>
2021-09-20 20:25:43 -0300

    teste

---
+++ Fabiano_Projeto_Java/src/OperadoresArit.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+
+public class OperadoresArit {
+
+	public static void main(String[] args) {
+	
+		
+
+    Double valor1, valor2;
+	Double resultado = null;
+	Integer operacao;
+	String OP ="";
+	
+	valor1   = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor .:"));
+	operacao = Integer.parseInt(JOptionPane.showInputDialog("Operação [1 = + | 2 = - | 3 = * | 4 = /]: "));
+	valor2   = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor .:"));
+	           
+	 if (operacao.equals(1)){
+	   	resultado = valor1+valor2;
+	   	OP = "Soma";
+	 }
+	 
+	 if (operacao.equals(2)) {
+		 resultado = valor1 - valor2;
+		 OP = "Subtração";
+ 	 }
+	 
+	 if (operacao.equals(3)) {
+		 resultado = valor1 * valor2;
+		 OP = "Multiplicação";
+	 }
+		
+	 if(operacao.equals(4)) {
+		 resultado = valor1 / valor2;
+		 OP = "Divisão";
+	 }
+		
+	   JOptionPane.showMessageDialog(null,"Resultado da operação de "+ OP+" : " + resultado);
+	
+	   //showMessageDialog(null,
+	 //System.out.println("Resultado da operação "+resultado) ; 
+	 
+	 
+	 
+  }
+
+}
